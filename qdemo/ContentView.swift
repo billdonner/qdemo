5f1854535d3e26d935496cddd54e233f6d2621a9
bill donner <billdonner@gmail.com>
2024-04-13 18:37:08 -0500

    push for carol

--- qdemo/ContentView.swift
+++ qdemo/ContentView.swift
@@ -5,9 +5,12 @@
 //  Created by bill donner on 4/9/24.
 //
 import SwiftUI
+import q20kshare
 
+
+var questions:[String] = []
 // Define an array of pastel colors for the background
- let pastelColors: [Color] = [
+let pastelColors: [Color] = [
   Color(red: 0.98, green: 0.85, blue: 0.87),
   Color(red: 0.84, green: 0.98, blue: 0.85),
   Color(red: 0.86, green: 0.91, blue: 0.98),
@@ -20,69 +23,87 @@ import SwiftUI
 let formatter = NumberFormatter()
 // Convert number to words
 func convertNumberToWords(_ number: Int) -> String? {
-  return formatter.string(from: NSNumber(value: number))
+ let r =  formatter.string(from: NSNumber(value: number)) ?? ""
+ return  (r as NSString).replacingOccurrences(of: "-", with: " ")
+}
+// Convert number to question from q20k
+func convertNumberToQuestion(_ number: Int) -> String? {
+  guard number > 0 && number <= questions.count else {return nil}
+ return questions [number-1]
 }
 
 @Observable class AppSettings  {
-  var elementWidth: CGFloat = 60
-   var elementHeight: CGFloat = 60
-    var shaky: Bool = false
- var worded: Bool = false
- var rows: Double = 4
+  var elementWidth: CGFloat = 100
+  var elementHeight: CGFloat = 100
+  var shaky: Bool = false
+  var worded: Bool = false
+  var questions: Bool = true
+  var rows: Double = 4
   var columns: Double = 3
+  var fontsize: Double = 16
 }
 
+func boxCon (_ number:Int,settings:AppSettings) -> String {
+  if settings.questions {
+    let a =  convertNumberToQuestion(number)
+    if a != nil { return a! }
+  }
+  if settings.worded {
+    let b = convertNumberToWords(number)
+    if b != nil {return b! }
+  }
+    return "\(number)"
+  }
 struct MatrixItem: View {
-    let number: Int
-    let backgroundColor: Color
-    let settings:AppSettings
-    var onTap: (() -> Void)? // Closure to be executed on tap
-    
-    var body: some View {
-      let boxContents:String  = settings.worded ? (convertNumberToWords(number) ?? "\(number)") :
-      "\(number)"
-      
-        Text(boxContents)
-            .font(.system(size: 20))
-            .lineLimit(1)
-            .minimumScaleFactor(0.1)
-            .frame(width:settings.elementWidth, height: settings.elementHeight, alignment: .center)
-            .background(backgroundColor)
-            .foregroundColor(Color.black)
-            .rotationEffect(settings.shaky ? .degrees(Double( number % 23)) : .degrees(0))
-            .padding(1)
-            .onTapGesture {
-                onTap?() // Execute the closure if it exists
-            }
-    }
-
+  let number: Int
+  let backgroundColor: Color
+  let settings:AppSettings
+  var onTap: (() -> Void)? // Closure to be executed on tap
+  
+  
+  var body: some View {
+    Text(boxCon(number,settings: settings))
+      .font(.system(size:settings.fontsize))
+      .lineLimit(5)
+      .minimumScaleFactor(0.1)
+      .frame(width:settings.elementWidth, height: settings.elementHeight, alignment: .center)
+      .background(backgroundColor)
+      .foregroundColor(Color.black)
+      .rotationEffect(settings.shaky ? .degrees(Double( number % 23)) : .degrees(0))
+      .padding(2)
+      .onTapGesture {
+        onTap?() // Execute the closure if it exists
+      }
+  }
+  
 }
 
 struct DetailView: View {
-    let text: String
-    let backgroundColor: Color
-    
+  let selected:Int
+  let backgroundColor: Color
+  let settings:AppSettings
+  
   var body: some View {
     NavigationView {
       ZStack {
         backgroundColor.edgesIgnoringSafeArea(.all)
-        Text(text)
+        Text(boxCon(selected,settings:settings))
           .font(.largeTitle)
           .foregroundColor(.black) // Assuming the background is dark enough; adjust accordingly.
           .padding()
-      }.navigationTitle("Details for \(text)")
+      }.navigationTitle("Details for question \(selected)")
     }
   }
 }
 #Preview("Details") {
-  DetailView(text: "Now is the time for all good men to go home",
-             backgroundColor:.yellow)
+  DetailView(selected:1999,
+             backgroundColor:.yellow,settings:AppSettings())
 }
 
 struct OneRowView: View {
   let firstnum: Int
   let settings:AppSettings
-  @Binding var selected:String
+  @Binding var selected:Int
   @Binding var background:Color
   @Binding var isPresented:Bool
   var body: some View {
@@ -91,12 +112,12 @@ struct OneRowView: View {
     let upper = firstnum+Int(settings.rows+1)*Int(settings.columns)-delta
     HStack {
       //for number in lower..<upper {
-   ForEach(lower..<upper, id: \.self) { number in
+      ForEach(lower..<upper, id: \.self) { number in
         MatrixItem(number: number, backgroundColor: pastelColors[number % pastelColors.count],settings:settings) {
           // This block will be called when the item is tapped
-         selected = convertNumberToWords(number) ?? "\(number)"
+          selected = number
           background  = pastelColors[number % pastelColors.count]
-        isPresented = true
+          isPresented = true
         }
       }
     }
@@ -106,7 +127,7 @@ struct OneRowView: View {
 struct OuterMatrixView: View {
   let settings:AppSettings
   @State private var isSheetPresented = false
-  @State private var selectedItemText: String = ""
+  @State private var selected: Int = -1
   @State private var selectedItemBackgroundColor: Color = .clear
   
   
@@ -115,65 +136,67 @@ struct OuterMatrixView: View {
       VStack {
         ForEach(0..<Int(settings.rows), id: \.self) { row in
           OneRowView(firstnum:(row-1)*Int(settings.columns),
-                     settings:settings,selected:$selectedItemText,background:$selectedItemBackgroundColor, isPresented: $isSheetPresented)
+                     settings:settings,selected:$selected,background:$selectedItemBackgroundColor, isPresented: $isSheetPresented)
         }
       }
     }
     .sheet(isPresented: $isSheetPresented) {
-      DetailView(text: selectedItemText, backgroundColor: selectedItemBackgroundColor)
+      if selected > 0 {
+        DetailView(selected:selected, backgroundColor: selectedItemBackgroundColor,settings:settings)
+      }
     }
   }
 }
 #Preview ("Game"){
-OuterMatrixView(settings: AppSettings())
+  OuterMatrixView(settings: AppSettings())
 }
+let MAX_ROWS = 30.0
+let MAX_COLS = 30.0
+
 struct SettingsFormView: View {
-@Bindable var  settings: AppSettings
+  @Bindable var  settings: AppSettings
   @State private var isPresentingMainView = false
-    var body: some View {
-       // NavigationView {
-            Form {
-                Section(header: Text("Settings")) {
-                    VStack(alignment: .leading) {
-                      Text("ROWS Current: \(settings.rows, specifier: "%.0f")")
-                      Slider(value: $settings.rows, in: 1...20, step: 1.0)
-                
-                    }
-                    
-                    VStack(alignment: .leading) {
-                      Text("COLS Current: \(settings.columns, specifier: "%.0f")")
-                      Slider(value: $settings.columns, in: 1...20, step: 1.0)
-                      
-                    }
-                  VStack(alignment: .leading) {
-                    Text("WIDTH Current: \(settings.elementWidth, specifier: "%.0f")")
-                      Slider(value: $settings.elementWidth, in: 60...300, step: 1.0)
-                  }
-                  
-                  VStack(alignment: .leading) {
-                      Text("HEIGHT Current: \(settings.elementHeight, specifier: "%.0f")")
-                    Slider(value: $settings.elementHeight, in: 60...300, step: 1.0)
-                  }
-              }
-              Section(header: Text("Features")) {
-                  Toggle(isOn: $settings.shaky) {
-                    Text("Shaky")
-                }
-                
-                Toggle(isOn:$settings.worded) {
-                      Text("Worded")
-                  }
-              }
-                
-//                Button("Go To Demo") {
-//                    isPresentingMainView = true
-//                }.sheet(isPresented: $isPresentingMainView) {
-//                    ContentView(settings: settings)
-//                }
-            }
-           // .navigationBarTitle("Q20K Lab")
-      //  }
+  var body: some View {
+    // NavigationView {
+    Form {
+      Section(header: Text("Settings")) {
+        VStack(alignment: .leading) {
+          Text("ROWS Current: \(settings.rows, specifier: "%.0f")")
+          Slider(value: $settings.rows, in: 1...MAX_ROWS, step: 1.0)
+        }
+        VStack(alignment: .leading) {
+          Text("COLS Current: \(settings.columns, specifier: "%.0f")")
+          Slider(value: $settings.columns, in: 1...MAX_COLS, step: 1.0)
+        }
+        VStack(alignment: .leading) {
+          Text("WIDTH Current: \(settings.elementWidth, specifier: "%.0f")")
+          Slider(value: $settings.elementWidth, in: 60...300, step: 1.0)
+        }
+        VStack(alignment: .leading) {
+          Text("HEIGHT Current: \(settings.elementHeight, specifier: "%.0f")")
+          Slider(value: $settings.elementHeight, in: 60...300, step: 1.0)
+        }
+        VStack(alignment: .leading) {
+          Text("FONTSIZE Current: \(settings.fontsize, specifier: "%.0f")")
+          Slider(value: $settings.fontsize, in: 8...40, step: 2.0)
+        }
+      }
+      Section(header: Text("Features")) {
+        Toggle(isOn: $settings.shaky) {
+          Text("Shaky")
+        }
+        
+        Toggle(isOn:$settings.worded) {
+          Text("Worded")
+        }
+        
+        Toggle(isOn:$settings.questions) {
+          Text("Questions")
+        }
+      }
+      
     }
+  }
 }
 
 #Preview ("Settings"){
@@ -185,17 +208,48 @@ struct ContentView: View {
   init (settings:AppSettings) {
     formatter.numberStyle = .spellOut
     self.settings = settings
+    
   }
+  @State private var isLoaded = false
   var body: some View {
     NavigationView {
-      VStack{
-        SettingsFormView(settings: settings)
-        .background(.gray)
-          .frame(height:120)
-        
+      ZStack {
+        ProgressView().opacity(!isLoaded ? 1.0:0.0)
+        VStack{
+          SettingsFormView(settings: settings)
+            .frame(height:300)
+          Divider()
           OuterMatrixView(settings:settings)
+        }.opacity(isLoaded ? 1.0:0.0)
       }
-        .navigationTitle("Q20K Laboratory")
+      .navigationTitle("Q20K Laboratory")
+    }.navigationViewStyle(.stack)
+    .task {
+      do{
+        var gamedatum:[GameData] = []
+        let playdata = try await restorePlayDataURL(url)
+        if let playdata = playdata {
+          gamedatum = playdata.gameDatum
+          var r:[String] = []
+          var totq: Int = 0
+          //keep filling till all we can ever need
+          while totq < Int(MAX_ROWS*MAX_COLS){
+            for gd in gamedatum {
+              for a in gd.challenges {
+                r.append(a.question)
+                totq+=1
+              }
+            }
+          }
+          
+          questions = r
+          isLoaded = true
+          print(playdata.playDataId," now available")
+        }
+      }
+      catch {
+        print("Could not restore from \(url)")
+      }
     }
   }
 }
--- qdemo/qdemoApp.swift
+++ qdemo/qdemoApp.swift
@@ -6,7 +6,10 @@
 //
 
 import SwiftUI
+import q20kshare
 
+
+let url = URL(string:"https://billdonner.com/fs/gd/readyforios01.json")!
 @main
 struct qdemoApp: App {
  let  settings = AppSettings()
@@ -16,3 +19,36 @@ struct qdemoApp: App {
         }
     }
 }
+
+
+func downloadFile(from url: URL ) async throws -> Data {
+let (data, _) = try await URLSession.shared.data(from: url)
+return data
+}
+
+ func restorePlayDataURL(_ url:URL) async  throws -> PlayData? {
+do {
+  let start_time = Date()
+  let tada = try await  downloadFile(from:url)
+  let str = String(data:tada,encoding:.utf8) ?? ""
+  do {
+    let pd = try JSONDecoder().decode(PlayData.self,from:tada)
+    let elapsed = Date().timeIntervalSince(start_time)
+    print("************")
+    print("Downloaded \(pd.playDataId) in \(elapsed) secs from \(url)")
+    let challengeCount = pd.gameDatum.reduce(0,{$0 + $1.challenges.count})
+    print("Loaded"," \(pd.gameDatum.count) topics, \(challengeCount) challenges in \(elapsed) secs")
+    print("************")
+    return pd
+  }
+  catch {
+    print(">>> could not decode playdata from \(url) \n>>> original str:\n\(str)")
+  }
+}
+catch {
+  throw error
+}
+return nil
+}
+
+
